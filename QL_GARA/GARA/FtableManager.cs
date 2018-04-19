7d7875039bf0920d82f448aed6f4f24666185460
16520157 <16520157@gm.uit.edu.vn>
2018-04-20 00:31:22 +0700

    commit

--- QL_GARA/GARA/FtableManager.cs
+++ QL_GARA/GARA/FtableManager.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace GARA
 {
-    public partial class Form1 : Form
+    public partial class FtableManager : Form
     {
-        public Form1()
+        public FtableManager()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //,,
         }
     }
 }
