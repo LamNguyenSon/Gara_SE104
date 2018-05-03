b91e415e042240326ea70cd09e2afb39bd4bf497
lannguyen <16520650@gm.uit.edu.vn>
2018-05-03 15:39:09 +0700

    .

--- WIP/Source/Source/Home_page.Designer.cs
+++ WIP/Source/Source/Home_page.Designer.cs
@@ -117,7 +117,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(154)))), ((int)(((byte)(160)))));
-			this.ClientSize = new System.Drawing.Size(660, 469);
+			this.ClientSize = new System.Drawing.Size(660, 411);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button4);
 			this.Controls.Add(this.button3);
---
+++ WIP/Source/Source/Quản lý Nhân viên.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Source
+{
+	partial class QUẢN_LÝ_NHÂN_VIÊN
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button5 = new System.Windows.Forms.Button();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.SystemColors.Control;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(102, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Thông tin nhân viên";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 47);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(40, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Mã NV";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 88);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Họ Tên";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(64, 44);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(149, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(64, 85);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(149, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.panel1.Controls.Add(this.tableLayoutPanel1);
+			this.panel1.Location = new System.Drawing.Point(5, 201);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(789, 212);
+			this.panel1.TabIndex = 5;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 6;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.65445F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.34555F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 155F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 147F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 130F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 154F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 26);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 5;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.61728F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.38272F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 183);
+			this.tableLayoutPanel1.TabIndex = 0;
+			this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(300, 47);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(39, 13);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "CMND";
+			this.label4.Click += new System.EventHandler(this.label4_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(360, 44);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(149, 20);
+			this.textBox3.TabIndex = 7;
+			this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(64, 123);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(287, 20);
+			this.textBox4.TabIndex = 8;
+			this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(14, 126);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(41, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Địa Chỉ";
+			this.label5.Click += new System.EventHandler(this.label5_Click);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(300, 88);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(51, 13);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Giới Tính";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(360, 88);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(47, 17);
+			this.radioButton1.TabIndex = 11;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Nam";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(442, 88);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(39, 17);
+			this.radioButton2.TabIndex = 12;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Nữ";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 18);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(72, 26);
+			this.button1.TabIndex = 13;
+			this.button1.Text = "Thêm";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 93);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(72, 26);
+			this.button2.TabIndex = 14;
+			this.button2.Text = "Xóa";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 58);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(72, 26);
+			this.button3.TabIndex = 15;
+			this.button3.Text = "Cập nhật";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(90, 18);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(72, 26);
+			this.button4.TabIndex = 16;
+			this.button4.Text = "Tìm kiếm";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(24, 201);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(109, 13);
+			this.label7.TabIndex = 1;
+			this.label7.Text = "Danh sách nhân viên";
+			this.label7.Click += new System.EventHandler(this.label7_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.panel2.Controls.Add(this.textBox4);
+			this.panel2.Controls.Add(this.label5);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.textBox1);
+			this.panel2.Controls.Add(this.radioButton2);
+			this.panel2.Controls.Add(this.textBox2);
+			this.panel2.Controls.Add(this.label6);
+			this.panel2.Controls.Add(this.radioButton1);
+			this.panel2.Controls.Add(this.textBox3);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.label4);
+			this.panel2.Location = new System.Drawing.Point(168, 18);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(627, 177);
+			this.panel2.TabIndex = 17;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(90, 58);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(72, 26);
+			this.button5.TabIndex = 18;
+			this.button5.Text = "Thoát";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// QUẢN_LÝ_NHÂN_VIÊN
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 425);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "QUẢN_LÝ_NHÂN_VIÊN";
+			this.Text = "QUẢN LÝ NHÂN VIÊN";
+			this.panel1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+	}
+}
