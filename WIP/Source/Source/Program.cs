8889a5307dbaae8f48a0d3a08aa5c21d4a656ca3
16520157 <16520157@gm.uit.edu.vn>
2018-05-07 13:57:01 +0700

    addd

--- WIP/Source/Source/Program.cs
+++ WIP/Source/Source/Program.cs
@@ -15,7 +15,7 @@ namespace Source
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(false);//
             Application.Run(new fLogin());
         }
     }
